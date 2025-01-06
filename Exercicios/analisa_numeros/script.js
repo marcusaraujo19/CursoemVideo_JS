42107ee93dc075cb8f07b18a4a78f3bee0306998
marcusaraujo19 <marcus.araujo18@etec.sp.gov.br>
2025-01-06 19:51:12 -0300

    criando controle do exercicio

--- Exercicios/analisa_numeros/script.js
+++ Exercicios/analisa_numeros/script.js
@@ -1,20 +1,58 @@
 // Declaração de VAR
-
 let caixa_dados = document.getElementById('resultado');
 let btn = document.getElementById('btnAdd');
 let vetNumeros = [];
 let exibir_val = document.createElement('div');
 exibir_val.setAttribute('id', 'exibir_val');
 
+btn.addEventListener('mouseenter', entrar)
+btn.addEventListener('mouseout', sair)
+
+function entrar(){
+    btn.style.color = 'blue'
+}
+function sair(){
+    btn.style.color = 'black'
+}
+
 function adicionar() {
     //Declaração de VAR
     let valor = Number(document.getElementById('txtvalor').value);
-    //AÇÕES
-    vetNumeros.push(valor + ' Valor adicionado <br>');
-    exibir_val.innerHTML = vetNumeros.join(' ');
+    //Verifica se não existe mensagem de erro
+    let msgerro = document.getElementById('msgerro')
+    if (msgerro) {
+        msgerro.remove()
+    }
 
+    //VERIFICAÇÃO 
+    if(valor < 1 || valor > 100 ){
+        let msgerro = document.createElement('p')
+        msgerro.setAttribute('id', 'msgerro')
+        msgerro.innerHTML = 'Erro! Valor é incompatível'
+        msgerro.style.color = 'red'
+        exibir_val.innerHTML = ''
+        exibir_val.appendChild(msgerro)
+    }else{
+        //AÇÕES
+    vetNumeros.push(valor);
 
+       //CONSTRUINDO A STRING PARA EXIBIÇÃO
+    let resultado = ''
+    for(i = 0; i < vetNumeros.length; i++){
+        resultado += `${vetNumeros[i]} Valor adicionado <br>`
+        
+    }
+    let btnFinal = document.createElement('input')
+    btnFinal.setAttribute('type', 'button')
+    btnFinal.setAttribute('id', 'btnFinal')
+    btnFinal.addEventListener('click', resumo)
+    exibir_val.innerHTML = resultado
+    console.log(vetNumeros) 
+    }
 
+    //QUERO FRIZAR QUE PARA EXIBIR NA TELA UMA STRING USA APENAS O INNERHTML MAS SE FOR UMA TAG HTML PRECISA SER USADO APPENDCHILD
+    
+    //ROUPAGEM DA CAIXA DE SAÍDA DE DADOS
     exibir_val.style.width = '180px';
     exibir_val.style.backgroundColor = 'rgb(200, 224, 230)';
     exibir_val.style.margin = '10px';
