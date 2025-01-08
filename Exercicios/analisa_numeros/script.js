09103c317d70a4ec4fc540318fe366354cce0005
marcusaraujo19 <marcus.araujo18@etec.sp.gov.br>
2025-01-08 19:23:48 -0300

    criando saída de dados do btnFinal

--- Exercicios/analisa_numeros/script.js
+++ Exercicios/analisa_numeros/script.js
@@ -5,11 +5,17 @@ let vetNumeros = [];
 let exibir_val = document.createElement('div');
 exibir_val.setAttribute('id', 'exibir_val');
 
-//teste
+//DIV DO ESPAÇO DO BTNFINAL
 let div_abaixo_exib = document.createElement('div')
 div_abaixo_exib.setAttribute('id', 'div_abaixo_exib')
 
+//DIV DA RESUMO DOS DADOS ACIONADA COM O CLICK DO BTNFINAL
+let resumo = document.createElement('div')
+resumo.setAttribute('id', 'resumo') 
 
+/*-----------------------------------------------------------------------*/
+
+//FUNÇÕES DE EVENTOS PARA OS BOTÕES
 btn.addEventListener('mouseenter', entrar)
 btn.addEventListener('mouseout', sair)
 
@@ -18,7 +24,9 @@ function entrar(){
 }
 function sair(){
     btn.style.color = 'black'
+/*-----------------------------------------------------------------------*/
 }
+/*INIC. FUNÇÃO AQUI!!!*/
 
 function adicionar() {
     //Declaração de VAR
@@ -34,6 +42,7 @@ function adicionar() {
     let btnFinal = document.getElementById('btnFinal')
     if (btnFinal){
         btnFinal.remove()
+
     }
 
     //VERIFICAÇÃO 
@@ -65,24 +74,39 @@ function adicionar() {
     btnFinal.setAttribute('type', 'button')
     btnFinal.setAttribute('id', 'btnFinal')
     btnFinal.setAttribute('value', 'Finalizar')
-    //btnFinal.addEventListener('click', finalizar)
+    btnFinal.addEventListener('click', final) //ADICIONANDO ONCLICK DINAMICAMENTE
 
-    
     div_abaixo_exib.appendChild(btnFinal)
     
     }
+                                        //iNIC. FUNÇÃO AQUI!!!
+    function final(){
+        let verif_resumo = document.getElementById('resumo')
+        if(verif_resumo){
+            verif_resumo.remove()
+        }else{ 
+            let ordem = document.createElement('p')
+            ordem.setAttribute('id', 'ordem')
+            ordem.innerHTML = `Em ordem numérica ${vetNumeros.sort()}`
+            resumo.appendChild(ordem)
 
+            let maior_val = document.createElement('p')
+            maior_val.setAttribute('id', 'maior_val')
+            maior_val.innerHTML = `O maior número: ${Math.max(...vetNumeros)}`
+            resumo.appendChild(maior_val)
+                        
+        }
+        caixa_dados.appendChild(resumo)
+    }
+    
+    
+        
     //QUERO FRIZAR QUE PARA EXIBIR NA TELA UMA STRING USA APENAS O INNERHTML MAS SE FOR UMA TAG HTML PRECISA SER USADO APPENDCHILD
     
-    //ROUPAGEM DA CAIXA DE SAÍDA DE DADOS
-    exibir_val.style.width = '180px';
-    exibir_val.style.backgroundColor = 'rgb(200, 224, 230)';
-    exibir_val.style.margin = '10px';
-    exibir_val.style.textAlign = 'center'
-    
-    // Alterar a cor do botão
-    //btn.style.color = 'red';
     caixa_dados.appendChild(exibir_val);
     caixa_dados.appendChild(div_abaixo_exib)
+    
+    
+    
 }
  
